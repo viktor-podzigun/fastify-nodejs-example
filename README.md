38fb77b6d169744aa8cb484471d1554514e18251
Viktor Podzigun <viktor.podzigun@gmail.com>
2026-03-21 20:11:10 +0100

    Added server.js

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # fastify-nodejs-example
+
 Example CRUD API using Fastify HTTP framework
+
+## How to run
+
+Run the api server in dev/refresh mode:
+
+```bash
+npm run start:dev
+```
---
+++ src/server.js
@@ -0,0 +1,23 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async () => {
+  return { hello: "world" };
+});
+
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
